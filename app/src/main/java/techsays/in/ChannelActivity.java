9e4f7d0e5d5b5c969e3250d5b070408635633f4d
daines <daineschacko05@gmail.com>
2020-05-21 14:33:46 +0530

    youtube

--- app/src/main/java/techsays/in/ChannelActivity.java
+++ app/src/main/java/techsays/in/ChannelActivity.java
@@ -1,75 +1,40 @@
 package techsays.in;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.view.View;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-
 import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-
-public class Youtube extends AppCompatActivity {
-
+public class ChannelActivity extends AppCompatActivity {
     ListView lvVideo;
     ArrayList<VideoDetails> videoDetailsArrayList;
     CustomListAdapter customListAdapter;
     String searchName;
     String TAG="ChannelActivity";
-    String URL="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHFRKabY2dAIdMOwNOL2mGQ&maxResults=25&key=AIzaSyDjW3rNTNYzPtGKwKVWTl8q3w6N5EYG3hE";
-    private static final int ACTIVITY_NUM = 3;
-
- //   private static final String TAG = "SearchActivity";
-    private Context mContext = Youtube.this;
-
+    String URL="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8gdf1Tj0G600ZATg00QMsA&maxResults=25&key=AIzaSyDjW3rNTNYzPtGKwKVWTl8q3w6N5EYG3hE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.youtube);
+        setContentView(R.layout.activity_channel);
         lvVideo=(ListView)findViewById(R.id.videoList);
         videoDetailsArrayList=new ArrayList<>();
-        customListAdapter=new CustomListAdapter(Youtube.this,videoDetailsArrayList);
+        customListAdapter=new CustomListAdapter(ChannelActivity.this,videoDetailsArrayList);
         showVideo();
-        setupBottomNavigationView();
-
-
-}
-
-
-
-
-
-
-    private void setupBottomNavigationView() {
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-        BottomNavigationView bottomNavigationViewEx = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
     }
     private void showVideo() {
         RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
@@ -110,7 +75,4 @@ public class Youtube extends AppCompatActivity {
         stringRequest.setRetryPolicy(policy);
         requestQueue.add(stringRequest);
     }
-
 }
-
-
---
+++ app/src/main/java/techsays/in/VideoDetails.java
@@ -0,0 +1,32 @@
+package techsays.in;
+
+class VideoDetails {
+    String VideoName;
+    String VideoDesc;
+    String URL;
+    String VideoId;
+    public void setVideoName(String VideoName){
+        this.VideoName=VideoName;
+    }
+    public String getVideoName(){
+        return VideoName;
+    }
+    void setVideoDesc(String VideoDesc){
+        this.VideoDesc=VideoDesc;
+    }
+    public String getVideoDesc(){
+        return VideoDesc;
+    }
+    public void setURL(String URL){
+        this.URL=URL;
+    }
+    public String getURL(){
+        return URL;
+    }
+    public void setVideoId(String VideoId){
+        this.VideoId=VideoId;
+    }
+    public String getVideoId(){
+        return VideoId;
+    }
+}
